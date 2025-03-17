3c4108286128cc4699b6ba5c0b41f3802c8ac8a2
Noah Matthieu Abi Chahla <noah-matthieu.abi-chahla@epita.fr>
2025-03-16 17:03:23 -0700

    final

--- Instagram340.cpp
+++ Instagram340.cpp
@@ -2,65 +2,67 @@
 #include "Reel.h"
 #include "Story.h"
 #include <iostream>
+#include <memory>
 
-// =============== Big 3 =====================
+// ============= BIG 3 =============
 
 // Constructeur par défaut
-Instagram340::Instagram340() 
+Instagram340::Instagram340()
     : currentUser(nullptr)
 {
 }
 
 // Destructeur
 Instagram340::~Instagram340() {
-    // shared_ptr se détruit automatiquement
+    // Rien de particulier, les smart pointers se gèrent seuls
 }
 
 // Constructeur de copie
 Instagram340::Instagram340(const Instagram340& other) {
-    // Copie du currentUser (shared_ptr = copie de la référence partagée)
+    // Copie superficielle des shared_ptr
+    // (si tu veux un clonage profond, il faudrait dupliquer les Users)
     currentUser = other.currentUser;
+    allUsers = other.allUsers;
 }
 
-// Opérateur d’affectation
+// Opérateur d'affectation
 Instagram340& Instagram340::operator=(const Instagram340& other) {
     if (this != &other) {
         currentUser = other.currentUser;
+        allUsers = other.allUsers;
     }
     return *this;
 }
 
-// =============== Surcharge << =====================
+// ============= Surcharge << =============
 std::ostream& operator<<(std::ostream& os, const Instagram340& insta) {
     os << "Welcome to Instagram 340!\n";
     return os;
 }
 
-// =============== runInstagram() ===================
+// ============= runInstagram() =============
+//
+// Atterrit d'abord sur un "landing page" : 
+// (1) Sign Up, (2) Log In, (0) Exit
+// Quand un user est loggé, on appelle showMainMenu().
 void Instagram340::runInstagram() {
     // Affiche le message via la surcharge <<
     std::cout << *this;
 
     bool done = false;
     while (!done) {
-        std::cout << "\nMenu:\n"
-                  << "1. Create Profile\n"
-                  << "2. Display Profile\n"
-                  << "3. Modify Password\n"
-                  << "4. Create Post\n"
-                  << "5. Display All Posts\n"
-                  << "6. Display K-th Post\n"
-                  << "7. Modify Post Title\n"
-                  << "8. Edit Post\n"
-                  << "9. Delete Post\n"
-                  << "10. Quit\n"
+        // Landing Page menu
+        std::cout << "\nLanding Page:\n"
+                  << "1. Sign Up (create account)\n"
+                  << "2. Log In\n"
+                  << "0. Exit\n"
                   << "Enter choice: ";
         int choice;
         std::cin >> choice;
 
         switch (choice) {
         case 1: {
-            // Create Profile
+            // Sign Up
             std::string uName, em, pw, b, pic;
             std::cout << "Enter username: ";
             std::cin >> uName;
@@ -74,37 +76,100 @@ void Instagram340::runInstagram() {
             std::cout << "Enter profile pic: ";
             std::cin >> pic;
 
-            // On crée un nouvel User en shared_ptr
-            currentUser = std::make_shared<User>(uName, em, pw, b, pic);
+            // On crée un nouvel User
+            auto newUser = std::make_shared<User>(uName, em, pw, b, pic);
+            // On l'ajoute à la liste globale
+            allUsers.push_back(newUser);
+            // On se logge automatiquement dessus
+            currentUser = newUser;
+
+            // On affiche le menu principal
+            showMainMenu();
+            // Quand showMainMenu() se termine, on revient ici
             break;
         }
         case 2: {
-            // Display Profile
-            if (currentUser) {
-                currentUser->displayUserInfo();
-            } else {
-                std::cout << "No user profile created yet.\n";
+            // Log In
+            if (allUsers.empty()) {
+                std::cout << "No accounts exist yet. Please sign up first.\n";
+                break;
             }
+            // Affiche la liste des comptes
+            std::cout << "Accounts:\n";
+            for (std::size_t i = 0; i < allUsers.size(); i++) {
+                std::cout << (i + 1) << ". " 
+                          << allUsers[i]->getUsername() 
+                          << "\n";
+            }
+            int idx;
+            std::cout << "Enter the account number: ";
+            std::cin >> idx;
+            if (idx < 1 || (unsigned)idx > allUsers.size()) {
+                std::cout << "Invalid choice.\n";
+                break;
+            }
+
+            // On récupère le user choisi
+            currentUser = allUsers[idx - 1];
+            std::cout << "Logged in as " << currentUser->getUsername() << "\n";
+
+            // On affiche le menu principal
+            showMainMenu();
+            break;
+        }
+        case 0:
+            // Exit program
+            done = true;
+            break;
+        default:
+            std::cout << "Invalid choice.\n";
+            break;
+        }
+    }
+
+    std::cout << "Exiting Instagram340.\n";
+}
+
+// ============= showMainMenu() =============
+//
+// N'est appelé que s'il y a un currentUser.
+// Boucle sur les options 1..9 + "0" pour LOG OUT
+void Instagram340::showMainMenu() {
+    if (!currentUser) {
+        std::cout << "Error: no user is logged in.\n";
+        return;
+    }
+
+    bool exitMenu = false;
+    while (!exitMenu) {
+        std::cout << "\nMain Menu (User: " << currentUser->getUsername() << ")\n"
+                  << "1. Display Profile\n"
+                  << "2. Modify Password\n"
+                  << "3. Create Post\n"
+                  << "4. Display All Posts\n"
+                  << "5. Display K-th Post\n"
+                  << "6. Modify Post Title\n"
+                  << "7. Edit Post\n"
+                  << "8. Delete Post\n"
+                  << "0. Log Out\n"
+                  << "Enter choice: ";
+        int choice;
+        std::cin >> choice;
+
+        switch (choice) {
+        case 1: {
+            currentUser->displayUserInfo();
+            break;
+        }
+        case 2: {
+            std::string newPw;
+            std::cout << "Enter new password: ";
+            std::cin >> newPw;
+            currentUser->setPassword(newPw);
             break;
         }
         case 3: {
-            // Modify Password
-            if (currentUser) {
-                std::string newPw;
-                std::cout << "Enter new password: ";
-                std::cin >> newPw;
-                currentUser->setPassword(newPw);
-            } else {
-                std::cout << "No user profile created yet.\n";
-            }
-            break;
-        }
-        case 4: {
             // Create Post
-            if (!currentUser) {
-                std::cout << "No user profile created yet.\n";
-                break;
-            }
             std::cout << "Enter 1 for Reel, 2 for Story: ";
             int postType;
             std::cin >> postType;
@@ -128,81 +193,60 @@ void Instagram340::runInstagram() {
             currentUser->createPost(newPost);
             break;
         }
-        case 5: {
+        case 4:
             // Display All Posts
-            if (currentUser) {
-                currentUser->displayAllPosts();
-            } else {
-                std::cout << "No user profile created yet.\n";
-            }
+            currentUser->displayAllPosts();
+            break;
+        case 5: {
+            // Display K-th Post
+            int k;
+            std::cout << "Enter K: ";
+            std::cin >> k;
+            currentUser->displayKthPost(k);
             break;
         }
         case 6: {
-            // Display K-th Post
-            if (currentUser) {
-                int k;
-                std::cout << "Enter K: ";
-                std::cin >> k;
-                currentUser->displayKthPost(k);
-            } else {
-                std::cout << "No user profile created yet.\n";
+            // Modify Post Title
+            int k;
+            std::string newT;
+            std::cout << "Enter Post index: ";
+            std::cin >> k;
+            std::cout << "Enter new title: ";
+            std::cin.ignore();
+            std::getline(std::cin, newT);
+            if (!currentUser->modifyPostTitle(k, newT)) {
+                std::cout << "Unable to modify post at index " << k << "\n";
             }
             break;
         }
         case 7: {
-            // Modify Post Title
-            if (currentUser) {
-                int k;
-                std::string newT;
-                std::cout << "Enter Post index: ";
-                std::cin >> k;
-                std::cout << "Enter new title: ";
-                std::cin.ignore();
-                std::getline(std::cin, newT);
-                if (!currentUser->modifyPostTitle(k, newT)) {
-                    std::cout << "Unable to modify post at index " << k << "\n";
-                }
-            } else {
-                std::cout << "No user profile created yet.\n";
+            // Edit Post
+            int k;
+            std::cout << "Enter Post index to edit: ";
+            std::cin >> k;
+            if (!currentUser->editPost(k)) {
+                std::cout << "Unable to edit post at index " << k << "\n";
             }
             break;
         }
         case 8: {
-            // Edit Post
-            if (currentUser) {
-                int k;
-                std::cout << "Enter Post index to edit: ";
-                std::cin >> k;
-                if (!currentUser->editPost(k)) {
-                    std::cout << "Unable to edit post at index " << k << "\n";
-                }
-            } else {
-                std::cout << "No user profile created yet.\n";
-            }
-            break;
-        }
-        case 9: {
             // Delete Post
-            if (currentUser) {
-                int k;
-                std::cout << "Enter Post index to delete: ";
-                std::cin >> k;
-                if (!currentUser->deletePost(k)) {
-                    std::cout << "Unable to delete post at index " << k << "\n";
-                }
-            } else {
-                std::cout << "No user profile created yet.\n";
+            int k;
+            std::cout << "Enter Post index to delete: ";
+            std::cin >> k;
+            if (!currentUser->deletePost(k)) {
+                std::cout << "Unable to delete post at index " << k << "\n";
             }
             break;
         }
-        case 10:
-            done = true;
+        case 0:
+            // Log Out
+            currentUser = nullptr;
+            exitMenu = true; // on quitte le menu -> retour au landing page
             break;
         default:
             std::cout << "Invalid choice.\n";
             break;
         }
     }
-
-    std::cout << "Exiting Instagram340.\n";
 }
--- Instagram340.h
+++ Instagram340.h
@@ -2,25 +2,40 @@
 #define INSTAGRAM340_H
 
 #include <memory>
+#include <vector>
 #include <iostream>
 #include "User.h"
 
+/**
+ * Instagram340:
+ *  - Gère désormais une collection de User (allUsers) pour "sign up / log in".
+ *  - Stocke l'utilisateur courant (currentUser).
+ */
 class Instagram340 {
 private:
-    // Stockage du currentUser en smart pointer
+    // L'utilisateur actuellement connecté (ou nullptr si aucun)
     std::shared_ptr<User> currentUser;
 
+    // Liste globale de tous les comptes créés (Sign Up)
+    std::vector<std::shared_ptr<User>> allUsers;
+
+    // Affiche le menu principal si quelqu'un est connecté
+    void showMainMenu();
+
 public:
-    // Big 3
+    // Big 3 (déjà exigé dans le devoir)
     Instagram340();
-    ~Instagram340();                                     // destructeur
-    Instagram340(const Instagram340& other);             // constructeur de copie
-    Instagram340& operator=(const Instagram340& other);  // opérateur d’affectation
+    ~Instagram340();
+    Instagram340(const Instagram340& other);
+    Instagram340& operator=(const Instagram340& other);
 
-    // Fonction principale
-    void runInstagram(); 
+    // Point d'entrée de l'application
+    // Désormais: propose un "landing page" sign up / log in, 
+    // puis (si on se log) le menu principal
+    void runInstagram();
 
-    // Surcharge de l’opérateur << (Partie 2 : “Welcome to Instagram 340!”)
+    // Surcharge de l'opérateur <<
+    // pour afficher "Welcome to Instagram 340!"
     friend std::ostream& operator<<(std::ostream& os, const Instagram340& insta);
 };
 
