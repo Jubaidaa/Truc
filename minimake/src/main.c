06f826c72f0780e774aad820352dc301d12cf0f7
Noah Matthieu Abi Chahla <noah.abichahlapro@gmail.com>
2025-10-31 15:13:32 +0100

    tetet

--- minimake/src/main.c
+++ minimake/src/main.c
@@ -20,15 +20,11 @@ static void print_help(void)
 static const char *detect_makefile(void)
 {
     if (access("Makefile", F_OK) == 0)
-    {
         return "Makefile";
-    }
     if (access("makefile", F_OK) == 0)
-    {
         return "makefile";
-    }
-    fprintf(stderr, "No Makefile found\n");
-    exit(1);
+    fprintf(stderr, "Cannot open Makefile: No such file or directory\n");
+    exit(2);
 }
 
 static void pretty_print(void)
@@ -41,9 +37,7 @@ static void pretty_print(void)
     }
     char buf[4096];
     while (fgets(buf, sizeof(buf), f))
-    {
         fputs(buf, stdout);
-    }
     fclose(f);
 }
 
@@ -54,23 +48,17 @@ static int build_targets(struct rule *rules, struct variable *vars,
     if (start == -1)
     {
         if (rules)
-        {
             return build_rule(rules, vars, rules->target);
-        }
         fprintf(stderr, "No default target found.\n");
         return 1;
     }
     for (int i = start; i < argc; i++)
     {
         if (argv[i][0] == '-')
-        {
             continue;
-        }
         ret = build_rule(rules, vars, argv[i]);
         if (ret)
-        {
             break;
-        }
     }
     return ret;
 }
@@ -80,7 +68,7 @@ int main(int argc, char **argv)
     const char *makefile = NULL;
     int parse_only = 0;
     int first_target = -1;
-    
+
     for (int i = 1; i < argc; i++)
     {
         if (!strcmp(argv[i], "-h"))
@@ -104,41 +92,50 @@ int main(int argc, char **argv)
             continue;
         }
         if (first_target == -1)
-        {
             first_target = i;
-        }
     }
-    
+
     if (!makefile)
-    {
         makefile = detect_makefile();
+
+    if (access(makefile, F_OK) != 0)
+    {
+        fprintf(stderr, "Cannot open %s: %s\n", makefile, strerror(errno));
+        return 2;
     }
-    
+
     if (parse_file(makefile))
     {
-        fprintf(stderr, "Parser failed on %s\n", makefile);
+        /* Do NOT print "parser failed" if file doesn't exist */
+        if (access(makefile, F_OK) != 0)
+            return 2;
         return 1;
     }
-    
+
     if (parse_only)
     {
         pretty_print();
         return 0;
     }
-    
+
     struct variable *vars = NULL;
     struct rule *rules = NULL;
-    
+
     if (parse_rule_file("rule.txt", &vars, &rules))
     {
         fprintf(stderr, "cannot parse rule.txt\n");
+        free_variables(vars);
+        free_rules(rules);
         return 1;
     }
-    
+
     int ret = build_targets(rules, vars, argv, first_target, argc);
-    
-    free_variables(vars);
-    free_rules(rules);
-    
+
+    if (rules && !rules->visiting)
+    {
+        free_variables(vars);
+        free_rules(rules);
+    }
+
     return ret;
 }
