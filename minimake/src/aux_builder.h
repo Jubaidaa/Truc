668aa0804b703e8f943ca072b4b4eeb435cf6abb
Noah Matthieu Abi Chahla <noah.abichahlapro@gmail.com>
2025-11-18 10:41:44 +0100

    new proj base

--- minimake/src/aux_builder.h
+++
@@ -1,9 +0,0 @@
-#ifndef AUX_BUILDER_H
-#define AUX_BUILDER_H
-
-#include "builder.h"
-
-int parse_rule_file(const char *path, struct variable **vars, struct rule **rules);
-
-#endif /* ! AUX_BUILDER_H */
-
--- minimake/src/micro_shell.h
+++
@@ -1,6 +0,0 @@
-#ifndef MICRO_SHELL_H
-#define MICRO_SHELL_H
-
-int micro_shell(const char *cmd);
-
-#endif /* ! MICRO_SHELL_H */
