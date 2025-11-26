87c75741aefb39fbd3bf9a79e502c4e9f8efeb1f
VM_linux <noahkingdragonball@gmail.com>
2025-11-26 14:07:08 +0000

    trop de dinguerie on repars à 0

--- httpd/tests/check_errors.sh
+++ httpd/tests/check_errors.sh
@@ -2,13 +2,10 @@
 
 BINARY="./httpd"
 
-# Fonction pour tester une commande
 test_fail() {
     test_name="$1"
     shift
     echo -n "Test $test_name : "
-    
-    # Exécute le binaire avec les arguments donnés, redirige stderr vers /dev/null
     $BINARY "$@" 2>/dev/null
     ret=$?
     
@@ -21,7 +18,7 @@ test_fail() {
 
 echo "--- Vérification des codes de retour ---"
 
-# 1. Manque le Port (on met tout le reste)
+# 1. Manque le Port (
 test_fail "Missing PORT" --ip 127.0.0.1 --server-name test --root-dir . --pid-file test.pid
 
 # 2. Manque l'IP
@@ -36,6 +33,6 @@ test_fail "Missing ROOT_DIR" --port 8080 --ip 127.0.0.1 --server-name test --pid
 # 5. Manque le PID File
 test_fail "Missing PID_FILE" --port 8080 --ip 127.0.0.1 --server-name test --root-dir .
 
-# 6. Option inconnue (doit aussi renvoyer 2 selon getopt)
+# 6. Option inconnue (doit aussi renvoyer 2 sinon u ded)
 test_fail "Unknown Option" --port 8080 --ip 127.0.0.1 --server-name test --root-dir . --pid-file test.pid --toto
 
